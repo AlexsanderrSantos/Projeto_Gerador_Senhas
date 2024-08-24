933257ffffaeb0141170ba52758aadcc298ec1a7
Alek <alexsander.hma@gmail.com>
2024-08-23 22:37:56 -0300

    Introduzi funções a opcões
    coloquei funções a opções existentes

--- funcoes/funcoes_back.py
+++ funcoes/funcoes_back.py
@@ -1,3 +1,5 @@
+import random
+
 # printa linha de acordo com o texto
 def linha(tam = 42):
     print('-' * tam)
@@ -7,10 +9,30 @@ def titulo():
     print(f"Gerador de Senhas v.1".center(42))
     linha()
 
-def cabecalho(lst):
+
+def opcoesmenu():
+    list = ["Gerar senha 4 digitos", "Gerar senha 6 digitos", "Gerar senha 8 digitos"]
+    i = 1
+    for op in list:
+        print(f"{i}-{op}\n")
+        i += 1
+    
+    
+def cabecalho():
     titulo()
-    pos = 1
-    for i in lst:
-        print(f" {pos} - {i}\n")
-        pos += 1
-    resp = input("Digite sua opção: ")
+    opcoesmenu()
+    resp = int(input("Digite sua resposta: "))
+    gerarnumeros(resp)
+    
+def gerarnumeros(opc):
+    print(opc)
+    if opc == 1:
+        print(random.sample(range(0, 10), 4))
+    elif opc == 2:
+        print(random.sample(range(0,10), 6))
+    elif opc == 3:
+        print(random.sample(range(0,10), 8))
+    elif opc == 4:
+        return "break"
+        
+    

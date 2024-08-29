bfcdb382afaa8beef89ab2c256cb1d54a015d117
Alek <alexsander.hma@gmail.com>
2024-08-28 22:39:01 -0300

    Update_funcs
    updated somente_numeros and somente_letras.

--- Main.py
+++ Main.py
@@ -2,7 +2,4 @@ from funcoes import funcoes_back
 
 while True:
     opcao = funcoes_back.menu()
-    if opcao == 4:
-        print("volte sempre")
-        break
         
--- funcoes/funcoes_back.py
+++ funcoes/funcoes_back.py
@@ -13,50 +13,61 @@ def titulo():
 
 
 def opcoesmenu():
-    list = ["Criar senha númerica", "Criar senha alfanúmerica", "Criar senha alfabética", "Sair"]
+    list = ["Criar senha somente númerica", "Criar senha somente letras", "Criar senha somente alfanúmerica", "Sair"]
     i = 1
     for op in list:
         print(f"{i}-{op}\n")
         i += 1
+    resposta =(int(input("Digite sua resposta(1, 2, 3): ")))
+    if resposta == 1:
+        gerar_somentenumeros()
+    elif resposta == 2:
+        gerar_somenteletras()
+    elif resposta == 3:
+        gerar_caracteres()
+    else:
+        linha()
+        print("Opção invalida, tente 1, 2 ou 3")
     
-def gerar_numeros():
-    opcoes = [4,6,8]
-    print("Gostaria de uma senha com:\n")
+def gerar_somentenumeros():
+    linha()
+    print("Gostaria de uma senha com: ")
     print("4 Digitos - 6 Digitos - 8 Digitos: ")
     num = int(input())
-    if num not in opcoes:
-        print("Digite uma das 3 opções validas !")
-    else:
-        if num == 1:
-            return(random.sample(range(0, 10), num))
-        elif num == 2:
-            return(random.sample(range(0, 9),num))
-        elif num == 3:
-            return(random.sample(range(0, 9), num))
+    while True:
+        if num == 4:
+            print(random.sample(range(0, 10), num))
+        elif num == 6:
+            print(random.sample(range(0, 9),num))
+        elif num == 8:
+            print(random.sample(range(0, 9), num))
         else:
             print("Erro ! Digite somente uma das 3 opções.")
+        break
         
 
-def gerar_alfanumerica(tam=10):
+def gerar_somenteletras(quantidade = 8):
+    linha()
+    quantidade = int(input("Digite a quantidade de caracteres para sua senha[MIN 4 / MAX 18]: "))
+    linha()
     #string.ascii pega todas as letras a-z grande e pequena
     #string.digits pega todos os num de 0-9
-    caracteres = string.ascii_letters + string.digits
-    senha = ''.join(random.choice(caracteres) for _ in range(10))
-    return senha
+    while True:
+        if  4 <= quantidade <= 18:
+            caracteres = string.ascii_letters + string.digits
+            senha = ''.join(random.choice(caracteres) for _ in range(quantidade))
+            print(senha)
+        else:
+            print("Valor abaixo de do min ou maior que o maximo permitido, tente novamente.")
+        break
 
-def gerar_alfabetica():
+def gerar_caracteres():
     print("s")
     
 def menu():
     titulo()
-    opcoesmenu()
-    resp = int(input("Digite sua resposta: \n"))
-    if resp == 1:
-        print(gerar_numeros())
-    elif resp == 2:
-        print(gerar_alfanumerica(6))
-    elif resp == 3:
-        gerar_alfabetica()
+    n = opcoesmenu()
+    
 
 
         
